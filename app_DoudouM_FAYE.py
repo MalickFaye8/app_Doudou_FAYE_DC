a538a82dd96aee8a683e3b0fcd3de09f97d3fdf1
MalickFaye8 <97529843+MalickFaye8@users.noreply.github.com>
2025-02-10 18:51:24 +0000

    Update app_DoudouM_FAYE.py

--- app_DoudouM_FAYE.py
+++ app_DoudouM_FAYE.py
@@ -86,9 +86,9 @@ def scrape_data(urls, categories, num_pages):
                 #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
                 # }
 
-                headers = {
-                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
-                }
+                # headers = {
+                #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+                # }
                 headers = {
                     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
                     "Accept-Language": "en-US,en;q=0.9",
